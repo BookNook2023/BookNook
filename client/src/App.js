93a19b48725afb8a4233ca8a02e96f05b32d8964
PenguinTamer101 <shajian@uw.edu>
2023-08-04 21:32:31 -0700

    main page done!

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
+import book from './book.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>BOOK<span>NOOK</span></p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={book} className="App-book" alt="book" />
+        <button>Login</button>
+        <a href="url">Sign Up</a>
       </header>
     </div>
   );
