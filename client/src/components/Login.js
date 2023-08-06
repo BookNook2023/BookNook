1f60f1a553db29a13a01e95de8f4c3c1856f79bf
sais14 <sais14@uw.edu>
2023-08-05 22:02:51 -0700

    added submit event handler and saving username

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Login.css'; 
+
 
 //Need to add in event handlers such as handleSubmit and the value field to store whatever the user types
 //Also we need to talk about how to connect this back to the databse in terms of creating a new entry or accessing one
 const Login = () => {
+  const [username, setUsername] = useState('');
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log('Username:', username);
+  }
+
+  const handleUsernameChange = (evt) => {
+    setUsername(evt.target.value);
+  }
   return (
     <div className="App login-container">
       <h1>this is where you login! rainbows</h1>
-      <form >
+      <form onSubmit = {handleSubmit} >
         <input
           type="text"
           placeholder="Enter your username"
+          value = {username}
+          onChange = {handleUsernameChange}
         />
         <button type="submit">Login</button>
       </form>
