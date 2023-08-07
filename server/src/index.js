42a3844e5869b49e3f5549a0f601c1db01498cc4
sais14 <sais14@uw.edu>
2023-08-06 22:05:28 -0700

    added endpoint for logging in and return id if found

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const { Pool } = require("pg");
 require("dotenv").config();
 console.log("dotenv loaded");
@@ -12,30 +13,40 @@ console.log("DB_NAME:", process.env.DB_NAME);
 console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 console.log("DB_PORT:", process.env.DB_PORT);
 
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT,
-// });
-
-//Use this if .env doesn't work
-// Don't forget to change to your own info
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "test",
-  password: "(ADD YOUR OWN PASSWORD)",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
+
+
+app.use(bodyParser.json());
 
 app.get('/users', async (req, res) => {
   try {
-    const { rows } = await pool.query('SELECT * FROM users');
+    const { rows } = await pool.query('SELECT * FROM public.users');
     res.json(rows);
   } catch (error) {
     console.error('Error fetching users:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.get('/api/login', async (req, res) => {
+  try {
+    const {username} = req.query;
+    const queryText = 'SELECT id FROM public.users WHERE username=$1';
+    const result = await pool.query(queryText, [username]);
+    if (result.rows.length > 0) {
+      res.json({ id: result.rows[0].id });
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+
+  } catch (error) {
+    console.error('Error fetching username:', error);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
