4435faa0060721102172f2be40c2ad7a97a9f317
sais14 <sais14@uw.edu>
2023-08-07 22:23:01 -0700

    added api call for login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,15 +6,32 @@ import './Login.css';
 //Also we need to talk about how to connect this back to the databse in terms of creating a new entry or accessing one
 const Login = () => {
   const [username, setUsername] = useState('');
+  const [userId, setUserId] = useState(null);
 
-  const handleSubmit = (evt) => {
+  const handleSubmit = async (evt) => {
     evt.preventDefault();
-    console.log('Username:', username);
+    try {
+      console.log(username);
+      const response = await fetch(`/api/login?username=${username}`);
+      console.log(response);
+      //const data = await response.json();
+      //console.log(data);
+      if (response.ok) {
+        setUserId(response.id);
+        console.log(response.id);
+      } else {
+        setUserId(null);
+        console.log(response.id);
+      }
+    } catch {
+      console.error("Error: username not found");
+    }
   }
 
   const handleUsernameChange = (evt) => {
     setUsername(evt.target.value);
   }
+  
   return (
     <div className="App login-container">
       <h1>this is where you login! rainbows</h1>
@@ -27,6 +44,7 @@ const Login = () => {
         />
         <button type="submit">Login</button>
       </form>
+      {userId !== null && <p>Welcome, user with ID: {userId}</p>}
     </div>
   );
 };
